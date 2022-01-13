9ae7bf070230e26b52fb3efeeef5e8e203dc629a
Kiraa Corsac <wire150@gmail.com>
2022-01-13 13:35:32 +0100

    skeleton

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import FoodItem from './components/FoodItem';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Laco's Coffeeshop
+      </header>
+      <main className="App-main">
+        <FoodItem />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FoodItem.jsx
@@ -0,0 +1,7 @@
+import style from "./FoodItem.module.css";
+
+export default function FoodItem(props) {
+    return <div className={style.item}>
+        FoodItem
+    </div>
+}
