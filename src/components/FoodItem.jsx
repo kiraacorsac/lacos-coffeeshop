76bd29f314b0d26b28815f9401d7bb8431d17529
Kiraa Corsac <wire150@gmail.com>
2022-01-18 21:30:26 +0100

    FoodItem design

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -2,6 +2,18 @@ import style from "./FoodItem.module.css";
 
 export default function FoodItem(props) {
     return <div className={style.item}>
-        FoodItem
+
+        <img className={style.image} src="https://i.imgur.com/YBZacyX.jpeg" alt="test" />
+
+
+        <div className={style.content}>
+            <div className={style.buttons}>
+                <span>L</span>
+                <span>S</span>
+            </div>
+            <div className={style.name}>
+                FoodItem bla bla bla bla bla bla
+            </div>
+        </div>
     </div>
 }
