118d6ed1f76021df30a081718d63813de4e26267
Kiraa Corsac <wire150@gmail.com>
2022-01-25 13:41:54 +0100

    tag search implementation

--- src/components/TagSearch.jsx
+++ src/components/TagSearch.jsx
@@ -3,28 +3,48 @@ import style from "./TagSearch.module.css"
 
 
 //just a component
-// TODO: style
-// TODO: replace X with appropriate FA icon
 function Tag(props) {
     return <div className={style.tag}>
-        <span>X</span> Tag
+        <span>X</span> {props.tag}
     </div>
 }
 
 //also a component
+// TODO: clear input after adding tag
+// TODO: input tag on pressing enter (same as pressing the add tag button)
+// TODO: style everytnig
+// TODO (hard!): implement deleting tags when X is clicked on the tag 
 export default function TagSearch(props) {
     const [tagList, setTagList] = useState(["salat", "chicken", "tofu", "beef"])
+    const [searchedTag, setSearchedTag] = useState("");
+
+
+    function addToTagList(tag) {
+        let newTagList = tagList.slice();
+        newTagList.push(tag);
+        setTagList(newTagList);
+    }
+
+    function handleChange(e) {
+        setSearchedTag(e.target.value)
+    }
+
+
+    function handleClick(e){
+        addToTagList(searchedTag)
+    }
+
 
     const tagListRender = [];
     for (const tag of tagList) {
-        console.log(tag)
         tagListRender.push(
-            <Tag />
+            <Tag tag={tag} />
         )
     }
 
     return <div className={style.searchBox}>
-        <input type="text" className={style.tagInput} />
+        <input type="text" className={style.tagInput} value={searchedTag} onChange={handleChange} />
+        <button onClick={handleClick}>Add Tag</button>
         <div className={style.tagList}>
             {tagListRender}
         </div>
