d9ee5296cfe0f9140c4cf9c9daaf6dc4515a767c
Filka, Ladislav (lf925u) <lf925u@att.com>
2022-02-10 23:46:21 +0100

    change4

--- src/components/FoodItemList.jsx
+++ src/components/FoodItemList.jsx
@@ -5,8 +5,8 @@ import style from "./FoodItemList.module.css"
 export default function FoodItemList(props) {
 
     const foodItemListRender = []
-    
-
+    const foodTagsListRender = []
+    const filterTagsListRender = []
     //TODO: implement tag filtering
     //example:
     // tagFilter = { 'italian' }
@@ -24,16 +24,31 @@ export default function FoodItemList(props) {
     console.log(props.tagFilter)
     for (const food of props.data) {
         console.log(food.tags)
-        const foodTagsListRender = []
-        for (const tag of food.tags) {
-            foodTagsListRender.push(tag)
-        }
-       if (foodTagsListRender == props.tagFilter){
-        foodItemListRender.push(
+    // for ( let i=0 ; i< props.tagFilter.length ; ++i)
+    // {
+    //     for(var j=0 ; j<food.tags.length ; ++j) {
+    //         if(props.tagFilter[i] == food.tags[j]){
+    //             filterTagsListRender.push(filterTagsListRender[i])
+    //         } else if(props.tagFilter[i] != food.tags[j]){
+    //             return
+    //         }
+    //     }
+    // }
+    
+    // {foodItemListRender.push(
+    //     <FoodItem key={food.id} food={food}/>
+    // )}}
+
+    for (const foodTag of food.tags) {    
+    for (const filterTag of props.tagFilter) 
+        if (foodTag != filterTag) {return;} else {
+            filterTagsListRender.push(filterTag)
+        }  }
+       {foodItemListRender.push(
             <FoodItem key={food.id} food={food}/>
         )
-    }  
-}
+    }}
+
 
     return   <div className={style.foodItemList}>
     {foodItemListRender}
