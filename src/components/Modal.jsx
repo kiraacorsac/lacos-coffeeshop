fa6f9243f0ca8ee707764a61131eb30800b94732
KiraaCorsac <wire150@gmail.com>
2022-04-05 13:35:27 +0200

    fix modal window triggering close when content clicked

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,15 +2,19 @@ import style from "./Modal.module.css";
 
 //props.visible -> says if modal should appear
 export default function Modal(props) {
-  function onModalClose() {
+  function onModalClose(e) {
     props.setModalFlag(false);
+  }
+
+  function onModalContentClick(event) {
+    event.stopPropagation();
   }
 
   if (props.visible) {
     // fix onModalClose triggering when clicking on children of modal wrapper
     return (
       <div className={style.modalWrapper} onClick={onModalClose}>
-        <div className={style.modal}>{props.children}</div>
+        <div className={style.modal} onClick={onModalContentClick}>{props.children}</div>
       </div>
     );
   } else {
